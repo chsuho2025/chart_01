011a42d4117bb12798e3efce331adbb7991d5a63
chsuho2025 <chsuho2025@gmail.com>
2025-09-21 00:43:08 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/chart_01/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   server: {
     host: '0.0.0.0',
     port: 5000,
